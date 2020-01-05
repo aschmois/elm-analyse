f13ddc20b06bfdf5f91137a06e62d5cb2c00f846
Danny Zhu <dzhu@dzhu.us>
2020-01-05 17:57:23 +0600

    Add tests for new bool check functionality

--- tests/Analyser/Checks/UnnecessaryLiteralBoolsTests.elm
+++ tests/Analyser/Checks/UnnecessaryLiteralBoolsTests.elm
@@ -26,9 +26,62 @@ foo x =
     )
 
 
+redundantBooleanOperation : ( String, String, List MessageData )
+redundantBooleanOperation =
+    ( "redundantBooleanOperation"
+    , """module Bar exposing (..)
+
+foo =
+    x == True
+
+bar =
+    False || foo
+"""
+    , [ Data.init "foo"
+            |> Data.addRange "range"
+                { start = { row = 7, column = 5 }, end = { row = 7, column = 17 } }
+      , Data.init "foo"
+            |> Data.addRange "range"
+                { start = { row = 4, column = 5 }, end = { row = 4, column = 14 } }
+      ]
+    )
+
+
+singleRedundantBoolean : ( String, String, List MessageData )
+singleRedundantBoolean =
+    ( "singleRedundantBoolean"
+    , """module Bar exposing (..)
+
+foo x =
+    if condition then
+        x
+
+    else
+        False
+
+
+bar x =
+    if condition then
+        True
+
+    else
+        x
+"""
+    , [ Data.init "foo"
+            |> Data.addRange "range"
+                { start = { row = 12, column = 5 }, end = { row = 17, column = 1 } }
+      , Data.init "foo"
+            |> Data.addRange "range"
+                { start = { row = 4, column = 5 }, end = { row = 11, column = 1 } }
+      ]
+    )
+
+
 all : Test
 all =
     CTU.build "Analyser.Checks.UnnecessaryLiteralBools"
         UnnecessaryLiteralBools.checker
         [ redundantBoolean
+        , redundantBooleanOperation
+        , singleRedundantBoolean
         ]
